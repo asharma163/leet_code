8cb79b8661b9d34ccb395d4b7c7f9b69a467f6d9
intelcomAnshul <82455862+intelcomAnshul@users.noreply.github.com>
2021-05-29 01:10:46 -0400

    Revert "1523. Count Odd Numbers in an Interval Range"
    This reverts commit 4953dc11

--- leetcode/CountOddNumbersIntervalRange.java
+++
@@ -1,9 +0,0 @@
-public class CountOddNumbersIntervalRange {
-    public static void main(String[] args) {
-        int low = 8;
-        int high = 10;
-        if (low % 2 != 0) low--;
-        if (high % 2 != 0) high++;
-        System.out.println((high - low) / 2);
-    }
-}
