2ad833b5492cb612d21d0d1e80caaf367fa22bc5
intelcomAnshul <82455862+intelcomAnshul@users.noreply.github.com>
2021-05-29 01:14:25 -0400

    1437. Check If All 1's Are at Least Length K Places Away

---
+++ leetcode/CheckIfAllAreatLeastLengthKPlacesAway.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckIfAllAreatLeastLengthKPlacesAway {
+    public static void main(String[] args) {
+        int[] nums = {1,0,0,0,1,0,0,1};
+        int k = 2;
+        boolean flag = false;
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                list.add(i);
+            }
+        }
+        if(list.size() == 1 || list.size() == 0) {
+            flag = true;
+        } else {
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (Math.abs(list.get(i) - list.get(i + 1)) > k) {
+                    flag = true;
+                } else {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        System.out.println(flag);
+    }
+}
