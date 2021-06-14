590f134c8db38aec3a79439b41a8b729e11b841d
intelcomAnshul <82455862+intelcomAnshul@users.noreply.github.com>
2021-06-14 00:05:27 -0400

    Implemented BST & Inorder Traversal

---
+++ leetcode/BST.java
@@ -0,0 +1,92 @@
+import java.time.temporal.Temporal;
+
+public class BST {
+    public static void main(String[] args) {
+        Tree intTree = new Tree();
+        intTree.insert(25);
+        intTree.insert(20);
+        intTree.insert(15);
+        intTree.insert(27);
+        intTree.insert(30);
+        intTree.insert(29);
+        intTree.insert(26);
+
+        intTree.traverseInOrder();
+    }
+}
+
+class TreeNode {
+    private int data;
+    private TreeNode leftChild;
+    private TreeNode rightChild;
+
+    public void insert(int value) {
+        if (value == data) {
+            return;
+        }
+        if (value < data) {
+            if (leftChild == null) {
+                leftChild = new TreeNode(value);
+            } else leftChild.insert(value);
+        } else {
+            if (rightChild == null) {
+                rightChild = new TreeNode(value);
+            } else rightChild.insert(value);
+        }
+    }
+
+    public void traverseInOrder() {
+        if (leftChild != null) {
+            leftChild.traverseInOrder();
+        }
+        System.out.print(data + ", ");
+        if (rightChild != null) {
+            rightChild.traverseInOrder();
+        }
+    }
+
+    public TreeNode(int data) {
+        this.data = data;
+    }
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public TreeNode getLeftChild() {
+        return leftChild;
+    }
+
+    public void setLeftChild(TreeNode leftChild) {
+        this.leftChild = leftChild;
+    }
+
+    public TreeNode getRightChild() {
+        return rightChild;
+    }
+
+    public void setRightChild(TreeNode rightChild) {
+        this.rightChild = rightChild;
+    }
+}
+
+class Tree {
+    private TreeNode root;
+
+    public void insert(int value) {
+        if (root == null) {
+            root = new TreeNode(value);
+        } else {
+            root.insert(value);
+        }
+    }
+
+    public void traverseInOrder() {
+        if (root != null) {
+            root.traverseInOrder();
+        }
+    }
+}
