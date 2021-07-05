ec6ab76a64984845014549d0f6dc78ee8e25ca2a
intelcomAnshul <82455862+intelcomAnshul@users.noreply.github.com>
2021-07-04 21:03:08 -0400

    1913. Maximum Product Difference Between Two Pairs

---
+++ leetcode/MaximumProductDifferenceBetweenTwoPairs.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class MaximumProductDifferenceBetweenTwoPairs {
+    public static void main(String[] args) {
+        int[] nums = {5,6,2,7,4};
+        Arrays.sort(nums);
+        System.out.println((nums[nums.length - 1] * nums[nums.length - 2]) - nums[0] * nums[1]);
+    }
+}
